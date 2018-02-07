d72df391925e04fb96ab9be53e2d87f95cc2cb3a
clarkjoe <cla12026@byui.edu>
2018-02-07 10:22:51 -0700

    documentation

--- R/cutPoint.R
+++ R/cutPoint.R
@@ -4,32 +4,35 @@
 #' 1. AccountNumber -- Unique numeric identifier --
 #' 2. Date -- Daily consecutive --
 #' 3. Count -- Must include all real numbers --
-#'
+#' @param movAvg Moving Average -- Defaults to 21
+#' @param startQuant Quantile percentage to determine start date cutPoint -- Defaults to 0.2 
+#' @param movAvg_zeroDays Number of consecetive moving averages evaluated at zero to begin end date cutPoint process -- Defaults to 1
+#' @param dialBack Number of days to 'dialBack' to determine a new end date cutPoint -- Defaults to 0
 #' @return filtered tibble
 #' @export
 #'
 #' @examples test
-cut_Points <- function(data) {
+cut_Points <- function(data, movAvg = 21, startQuant = 0.2, movAvg_zeroDays = 1, dialBack = 0) {
   
   library(tidyverse)
   library(magrittr)
   library(pracma)
-  library(strucchange)
+  library(lubridate)
   
   data %>%
     filter(Cluster3 == "Dropped") %>%
     group_by(AccountNumber) %>%
+    arrange(Date) %>%
     # calculate startDate #
-    mutate(M_AVG = movavg(Count, 21, "s"),
-           quant = quantile(M_AVG[M_AVG > 0], probs = 0.2),
+    mutate(M_AVG = movavg(Count, movAvg, "s"),
+           quant = quantile(M_AVG[M_AVG > 0], probs = startQuant),
            startDate = min(Date[(M_AVG > quant)])) %>%
     filter(Date >= startDate) %>%
     # calculate temporary endDate of data (remove some trailing zeros)#
     mutate(tmp_reverseSign = if_else(M_AVG > 0, 0, 1, missing = NULL),
            tmp_cumSum = cumsum(tmp_reverseSign),
-           zeroDate = max(Date[tmp_cumSum < 10])) %>%
-    filter(Date <= zeroDate) %>%
-    # calculate true cutPoint/endDate date based on newly trimmed data #
-    mutate(endDate = Date[tail(breakpoints(M_AVG ~ Date)$breakpoints,1)]) %>%
-    filter(Date <= endDate)
+           zeroDate = max(Date[tmp_cumSum < movAvg_zeroDays]),
+           endDate = zeroDate - movAvg,
+           cutDate = endDate - dialBack) %>%
+  filter(Date <= cutDate)
 }
