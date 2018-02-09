1de16a02b3714993ddd9bf0c9b14ecb701ed5b08
clarkjoe <cla12026@byui.edu>
2018-02-09 11:07:28 -0700

    working on trelliscope

--- Testing/Joseph/trelliscope.R
+++ Testing/Joseph/trelliscope.R
@@ -23,22 +23,9 @@ rawData %<>%
   spread(key = Date, value = normCount)
 
 
-sc <- spark_connect(master = "local")
-data_tbl <- copy_to(sc, rawDailyProfilesAllNorm[,-1], "data", overwrite = TRUE)
-numClusters = c(10,25,50,75,100,150,200,500)
-out4 = list()
-for(i in 1:length(numClusters)) {
-  out4[[i]] <- ml_kmeans(data_tbl, ~., centers = numClusters[i])
-  cat(numClusters[i], " ")
-}
-
 rawData_tbl <- copy_to(sc, rawData %>%
                          ungroup() %>%
                          select(-AccountNumber), "rawData", overwrite = TRUE)
-
-rawData_tbl %>%
-  ml_kmeans(~., centers = 3)
-
 
 tic()
 set.seed(1234)
@@ -51,14 +38,64 @@ for(i in 1:length(numClusters)) {
 }
 toc()
 
-rawData_tbl$`20140928`
-
-bob <- rawData_tbl %>%
-  select(`20140928`, `20141005`) %>%
-  ml_kmeans(~., centers = 3)
 
 qplot(x = numClusters, y = unlist(lapply(out, function(x) x$cost))) + 
   geom_line() + 
   labs(x = "Number of Clusters", y = "Total W/in Sums of Squares") +
   theme_bw()
+
+
+
+mlKmeans <- rawData_tbl %>%
+  ml_kmeans(~., centers = 200)
+
+
+predicted <- sdf_predict(mlKmeans, rawData_tbl) #%>%
+  collect()
+  
+  
+bob <- as_tibble(predicted)
+
+bob %>%
+  select(`20171210`, AccountNumber) %>%
+  filter(AccountNumber == "000016")
+
+rawData %>%
+  select(`2017-12-10`, AccountNumber) %>%
+  filter(AccountNumber == "000016")
+
+jack <- bob %>%
+  select(-features) %>%
+  filter(prediction == 70) %>%
+  gather("Date", "Count", `20140928`:`20180204`) %>%
+  mutate(Date = ymd(Date)) %>%
+  group_by(prediction)
+
+
+
+bob %>%
+  select(-features) %>%
+  filter(prediction == 70) %>%
+  gather("Date", "Count", `20140928`:`20180204`) %>%
+  mutate(Date = ymd(Date)) %>%
+  group_by(prediction) %>%
+  filter(Count > 0) %>%
+  nest() %>%
+  mutate(
+    #cogs = map_cog(data, ~ data_frame(
+    #  mean = mean(.$Count)
+    #)),
+    panel = map_plot(data, ~ ggplot(., aes(x = Date, y = Count, group = AccountNumber)))
+                       #geom_line(aes(alpha = .25)))
+                       #theme_bw())
+  ) %>%
+  trelliscope("billybob5", self_contained = T, auto_cog = FALSE)
+  
+
+
+
+
+
+
+
 
