4e8da374b86bcbe39bde62243f18e24cd5f30b11
trentonpulsipher <trenton.pulsipher@henryschein.com>
2018-02-14 15:36:40 -0700

    valentines day

--- Testing/Trenton/clustering.R
+++ Testing/Trenton/clustering.R
@@ -10,13 +10,13 @@ library(dtwclust)
 library(tictoc)
 
 
-# tic()
-# ###  Read in Daily Profiles of eClaims submissions created in SQL table by Trenton  ###
-# sqlFilename <- 'dbo.eClaimsDailyProfilesExpanded'
-# cn <- odbcDriverConnect("Driver={SQL Server Native Client 11.0};Server=hspsdata.nt.local;Database=SupportReports;Uid=USHSI/trenton.pulsipher;Pwd=22AngelA;trusted_connection=yes;",
-#                        believeNRows = F)
-# d <- sqlFetch(cn, sqlFilename) # 4 mins, ~610 Mb sized object, ~39.8M rows
-# toc()
+tic()
+###  Read in Daily Profiles of eClaims submissions created in SQL table by Trenton  ###
+sqlFilename <- 'dbo.eClaimsDailyProfilesExpanded'
+cn <- odbcDriverConnect("Driver={SQL Server Native Client 11.0};Server=hspsdata.nt.local;Database=SupportReports;Uid=USHSI/trenton.pulsipher;Pwd=22AngelA;trusted_connection=yes;",
+                       believeNRows = F)
+d <- sqlFetch(cn, sqlFilename) # 4 mins, ~610 Mb sized object, ~39.8M rows
+toc()
 
 # saveRDS(d, file = "~/R/R_prjs/tsCogs/R_Data/rawDailyProfilesAll-20180206.rds")
 load("~/R/R_prjs/tsCogs/R_Data/rawDailyProfilesAll-20180206.rds")
