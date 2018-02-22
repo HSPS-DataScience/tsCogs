bc62f262318775b77928ed3661cfa1d82f74532d
trentonpulsipher <trenton.pulsipher@henryschein.com>
2018-02-22 15:33:46 -0700

    bad day

--- SQL_scripts/eClaimsDailyGeneration.sql
+++ SQL_scripts/eClaimsDailyGeneration.sql
@@ -17,19 +17,19 @@ WHERE nbr - 1 <= DATEDIFF(DAY, @startDate, @endDate);
 SELECT AccountNumber,
        CONVERT(DATE, [Create_Date]) AS ymd,
        COUNT(*) AS Count
-INTO [SupportReports].[dbo].[eClaimDailyProfiles]
-FROM [SupportReports].[dbo].[vwTrentonV2]
+INTO [SupportReports].[dbo].[eClaimDailyProfiles3]
+FROM [SupportReports].[dbo].[vwTrentonV3]
 WHERE Claim_State IN('ACCEPTED', 'SETTLED', 'SETTLED ')
 GROUP BY AccountNumber,
          CONVERT(DATE, [Create_Date]);
 
 
 /****** Script to generate SupportReports.dbo.eClaimDailyProfilesExpanded ******/
-
+DECLARE @startDate DATE= '20141001', @endDate DATE= GETDATE();
 SELECT dailyZeroAccount.AccountNumber,
        dailyZeroAccount.ymd,
        COALESCE(eClaimDailyProfiles.Count, 0) AS Count
-INTO [SupportReports].[dbo].[eClaimDailyProfilesExpanded]
+INTO [SupportReports].[dbo].[eClaimDailyProfilesExpanded3]
 FROM
 (
     SELECT AccountNumber,
@@ -39,13 +39,13 @@ FROM
     SELECT AccountNumber,
            @startDate AS minYMD,
            @endDate AS maxYMD
-    FROM [SupportReports].[dbo].[eClaimDailyProfiles]
+    FROM [SupportReports].[dbo].[eClaimDailyProfiles3]
     GROUP BY AccountNumber
 ) AS tmp
 CROSS JOIN [SupportReports].[dbo].[dailyZero] AS dailyZero
     WHERE dailyZero.ymd BETWEEN tmp.minYMD AND tmp.maxYMD
 ) AS dailyZeroAccount
-LEFT JOIN [SupportReports].[dbo].[eClaimDailyProfiles] AS eClaimDailyProfiles ON dailyZeroAccount.AccountNumber = eClaimDailyProfiles.AccountNumber
+LEFT JOIN [SupportReports].[dbo].[eClaimDailyProfiles3] AS eClaimDailyProfiles ON dailyZeroAccount.AccountNumber = eClaimDailyProfiles.AccountNumber
                                                                            AND dailyZeroAccount.ymd = eClaimDailyProfiles.ymd
 ORDER BY dailyZeroAccount.AccountNumber,
          dailyZeroAccount.ymd; 
