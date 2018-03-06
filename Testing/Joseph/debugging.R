c3d5b1a5fd1778a826b596178892a42ea250c19b
trentonpulsipher <trenton.pulsipher@henryschein.com>
2018-03-06 14:38:59 -0700

    another try

--- Testing/Joseph/debugging.R
+++ Testing/Joseph/debugging.R
@@ -3,7 +3,7 @@ library(tsCogs)
 # Preparation
 tic()
 bob <- rawData %>%
-  filter(AccountNumber %in% c("75815", "15300")) %>%
+#  filter(AccountNumber %in% c("75815", "15300")) %>%
   as.tbl() %>%
   mutate(AccountNumber = as.character(AccountNumber)) %>%
   rename(Date = ymd) %>%
@@ -47,11 +47,10 @@ toc()
 # Truth
 truthData <- clusterData %>%
   select(AccountNumber, prediction) %>%
-  mutate(Truth = "Healthy") %>%
-  #mutate(Truth = if_else(prediction %in% c(1:5,7,8,14,15,17,18,20,21,26,28,29,
-  #                                         31,32,34,35,37,38,40,45,49,51,54,56,57,59,
-  #                                         60,62:65,57:70,72,75:77,80,84,85,87,
-  #                                         94,98,99), "Healthy", "Dropped")) %>%
+  mutate(Truth = if_else(prediction %in% c(1:5,7,8,14,15,17,18,20,21,26,28,29,
+                                           31,32,34,35,37,38,40,45,49,51,54,56,57,59,
+                                           60,62:65,57:70,72,75:77,80,84,85,87,
+                                           94,98,99), "Healthy", "Dropped")) %>%
   select(AccountNumber, prediction, Truth) %>%
   group_by(AccountNumber) %>%
   slice(1)
@@ -59,10 +58,11 @@ truthData <- clusterData %>%
 # Cogs and Rejoin
 jack <- cutData %>%
   nest_todo() %>%
-  nest_append_interval(cutPtData, "years", 1) %>%
-  nest_append_interval(cutPtData, "months", 6) %>%
-  nest_append_interval(cutPtData, "months", 3) %>%
-  nest_append_interval(cutPtData, "weeks", 6) %>%
-  nest_append_interval(cutPtData, "days", 14) %>%
-  left_join(truthData, by = "AccountNumber")
+  nest_append_interval(cutData, "years", 1) %>%
+  nest_append_interval(cutData, "months", 6) %>%
+  nest_append_interval(cutData, "months", 3) %>%
+  nest_append_interval(cutData, "weeks", 6) %>%
+  nest_append_interval(cutData, "days", 14) %>%
+  left_join(truthData, by = "AccountNumber") %>%
+  group_by(AccountNumber)
   
